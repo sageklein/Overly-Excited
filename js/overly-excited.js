0aaf98357acf6d25e4ca92caad7a12b2cb322167
Sage Klein <sageklein@gmail.com>
2019-09-03 10:50:19 -0500

    added css

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -13,3 +13,98 @@ let buildMeUp = " "
 		console.log(buildMeUp)
     }
 }
+
+// concatenate sentence
+
+function addExcitement(theWordArray) {
+	let buildMeUp = "";
+
+	for (let i = 0; i < theWordArray.length; i++) {
+		buildMeUp = " " + buildMeUp + " " + theWordArray[i];
+		console.log(buildMeUp);
+		document.write(`${buildMeUp} <br>`);
+	}
+}
+
+addExcitement(sentence);
+
+// Trying again with exclamation points every 3 words
+
+function addExcitement2(theWordArray) {
+	let buildMeUp = "";
+
+	for (let i = 0; i < theWordArray.length; i++) {
+		if ((i + 1) % 3 === 0) {
+			buildMeUp = " " + buildMeUp + " " + theWordArray[i] + "!";
+			document.write(`${buildMeUp} <br>`);
+		} else buildMeUp = " " + buildMeUp + " " + theWordArray[i];
+		document.write(`${buildMeUp} <br>`);
+	}
+}
+
+addExcitement2(sentence);
+
+// add a punctuation argument
+
+function addExcitement3(theWordArray, a) {
+	let buildMeUp = "";
+
+	for (let i = 0; i < theWordArray.length; i++) {
+		if ((i + 1) % 3 === 0) {
+			buildMeUp = " " + buildMeUp + " " + theWordArray[i] + a;
+			document.write(`${buildMeUp} <br>`);
+		} else buildMeUp = " " + buildMeUp + " " + theWordArray[i];
+		document.write(`${buildMeUp} <br>`);
+	}
+}
+
+addExcitement3(sentence, ",");
+
+// add a punctuation argument + number of time to use it
+
+// Function to repeat a string x number of times
+
+function repeatStringNumTimes(string, times) {
+	if (times > 0) return string.repeat(times);
+	else return "";
+}
+
+// function to add the punctuation x number of times
+
+function addExcitement4(theWordArray, a, times) {
+	let buildMeUp = "";
+
+	for (let i = 0; i < theWordArray.length; i++) {
+		if ((i + 1) % 3 === 0) {
+			buildMeUp =
+				" " +
+				buildMeUp +
+				" " +
+				theWordArray[i] +
+				repeatStringNumTimes(a, times);
+			document.write(`${buildMeUp} <br>`);
+		} else buildMeUp = " " + buildMeUp + " " + theWordArray[i];
+		document.write(`${buildMeUp} <br>`);
+	}
+}
+
+addExcitement4(sentence, ".", 5);
+
+// function to add the punctuation x number of times
+
+const addExcitement5 = (theWordArray, a, times) => {
+	let buildMeUp = "";
+
+	for (let i = 0; i < theWordArray.length; i++) {
+		if ((i + 1) % 3 === 0) {
+			buildMeUp =
+				" " +
+				buildMeUp +
+				" " +
+				theWordArray[i] +
+				repeatStringNumTimes(a, times);
+			document.write(`${buildMeUp} <br>`);
+		} else buildMeUp = " " + buildMeUp + " " + theWordArray[i];
+		document.write(`${buildMeUp} <br>`);
+	}
+};
